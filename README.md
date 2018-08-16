671d16c50459c12c5eaa501e3fe40c56f4d29817
Conrado Buhrer <conrado@buhrer.net>
2018-08-16 10:12:05 -0300

    added react

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ meteor+react
 ============
 
 Udemy course https://www.udemy.com/meteor-react-tutorial/learn/v4/t/lecture/5011652?start=0
+
+Code: https://github.com/stephengrider/meteorcasts
+
+Last stop:
+https://www.udemy.com/meteor-react-tutorial/learn/v4/t/lecture/5161956?start=0
---
+++ images/client/main.js
@@ -0,0 +1,22 @@
+// Any JS in here is automatically ran for us
+
+// Import the React library
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a component
+
+const App = () => {
+  return (
+    <div>
+      React App #2
+    </div>
+  );
+};
+
+// Render the component to the screen
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.container'));
+})
