deaa78da3a0b28f44bee56aaa4f9d067dadd9b33
Conrado Buhrer <conrado@buhrer.net>
2018-08-18 16:25:55 -0300

    add image details

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,19 +1,22 @@
-// Create our image list component
-
-// Import React
-
 import React from 'react';
 
-// Create our component
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400/000/fff' },
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400/000/fff' },
+  { title: 'Mug', link: 'https://dummyimage.com/600x400/000/fff' }
+];
 
 const ImageList = () => {
+  const RenderedImages = IMAGES.map(function(image) {
+    return <ImageDetail image={image} />
+  })
   return (
     <ul>
-      <li>Image Detail</li>
+      {RenderedImages}
     </ul>
   );
 };
-
-// Export our component
 
 export default ImageList;
