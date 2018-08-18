828dfdc97bf6ab50ec425b3b0218642781546fe5
Conrado Buhrer <conrado@buhrer.net>
2018-08-18 16:38:18 -0300

    minimal syntax refactor

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-import ImageDetail from './image_detail';
+import ImageDetail from './image_detail'
 
 const IMAGES = [
   { title: 'Pen', link: 'https://dummyimage.com/600x400/000/fff' },
   { title: 'Pine Tree', link: 'https://dummyimage.com/600x400/000/fff' },
   { title: 'Mug', link: 'https://dummyimage.com/600x400/000/fff' }
-];
+]
 
 const ImageList = () => {
-  const RenderedImages = IMAGES.map(function(image) {
-    return <ImageDetail image={image} />
-  })
+  const RenderedImages = IMAGES.map( image =>
+    <ImageDetail key={image.title} image={image} />
+  )
+
   return (
     <ul className="media-list list-group">
       {RenderedImages}
     </ul>
-  );
-};
+  )
+}
 
-export default ImageList;
+export default ImageList
