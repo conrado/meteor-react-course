9fe488308a4d0b8214b3bf337834b612d100afb1
Conrado Buhrer <conrado@buhrer.net>
2018-08-18 19:44:36 -0300

    installed react meteor data container watcher libs

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,24 +1,22 @@
-import { Meteor } from 'meteor/meteor';
-import { Employees } from '../imports/collections/employees';
+import { Meteor } from 'meteor/meteor'
+import { Employees } from '../imports/collections/employees'
 
-import _ from 'lodash';
-import { image, helpers } from 'faker';
+import _ from 'lodash'
+import { image, helpers } from 'faker'
 
 Meteor.startup(() => {
-  // good place to generate data
-
-  // check if we need to generate data
-  // - does collection have any records?
-  const numberRecords = Employees.find({}).count();
-  console.log(numberRecords);
+  const numberRecords = Employees.find({}).count()
   if (!numberRecords) {
-    // no records, generate data...
     _.times(5000, () => {
-      const { name, email, phone } = helpers.createCard();
+      const { name, email, phone } = helpers.createCard()
       Employees.insert({
         name, email, phone,
         avatar: image.avatar()
-      });
-    });
+      })
+    })
   }
-});
+
+  Meteor.publish('employees', () => {
+    return Employees.find({}, { limit: 20 })
+  })
+})
