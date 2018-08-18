9f01207c711c9ae9adaff80008c5faf77e952a61
Conrado Buhrer <conrado@buhrer.net>
2018-08-18 20:13:33 -0300

    added employee detail component

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
 
-const EmployeeList = () => {
+import { Employees } from '../../imports/collections/employees'
+import EmployeeDetail from './employee_detail'
+
+const EmployeeList = (props) => {
+  // props.employees => an array of employee objects
   return (
     <div>
       <div className="employee-list">
-        Employee List
+        {props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />)}
       </div>
     </div>
   )
 }
 
-export default EmployeeList
+export default createContainer( () => {
+  // set up the subscription
+  Meteor.subscribe('employees')
+  // return an object, whatever we return will be sent to EmployeeList as props
+  return { employees: Employees.find({}).fetch() }
+}, EmployeeList)
