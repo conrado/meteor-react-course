8950586f593b6d7642a1686013de677313c6b134
Conrado Buhrer <conrado@buhrer.net>
2018-08-18 18:34:47 -0300

    added imagescore component

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -2,14 +2,9 @@ import React from 'react'
 
 import ImageDetail from './image_detail'
 
-const IMAGES = [
-  { title: 'Pen', link: 'https://dummyimage.com/600x400/000/fff' },
-  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400/000/fff' },
-  { title: 'Mug', link: 'https://dummyimage.com/600x400/000/fff' }
-]
-
-const ImageList = () => {
-  const RenderedImages = IMAGES.map( image =>
+const ImageList = (props) => {
+  const validImages = props.images.filter( image => !image.is_album);
+  const RenderedImages = validImages.map( image =>
     <ImageDetail key={image.title} image={image} />
   )
 
--- images/client/main.js
+++ images/client/main.js
@@ -1,24 +1,36 @@
 import axios from 'axios';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import imgurConfig from './config/imgur';
 import ImageList from './components/image_list';
 
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+  }
+
+  componentWillMount () {
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
+      headers: {
+        Authorization: 'Client-ID ' + imgurConfig.clientId
+      }
+    }).then( response => this.setState({ images: response.data.data }));
+    // NEVER DO THIS:
+    // this.state.images = [ {}, {} ]
+  }
+
+  render () {
+    return (
+      <div>
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
 };
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
-    headers: {
-      Authorization: 'Client-ID ' + imgurConfig.clientId
-    }
-  }).then(response => console.log(response));
-
 })
