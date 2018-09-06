e7618fac6c189ba01045810933e7b5989e19ee6e
Conrado Buhrer <conrado@buhrer.net>
2018-09-06 20:35:36 -0300

    added authentication

--- markbin/client/components/header.js
+++ markbin/client/components/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Accounts from './accounts'
 
 class Header extends Component {
 
@@ -10,7 +11,7 @@ class Header extends Component {
         </div>
         <ul className="nav navbar-nav">
           <li>
-            <a>Sign up</a>
+            <Accounts />
           </li>
           <li>
             <a>Create Bin</a>
