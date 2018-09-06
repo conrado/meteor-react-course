64791522476304d1711213e0b1a76abee4ec5177
Conrado Buhrer <conrado@buhrer.net>
2018-09-06 14:07:18 -0300

    added error message to user

--- shorten-my-link/client/components/link_create.js
+++ shorten-my-link/client/components/link_create.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 
 class LinkCreate extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { error: '' }
+  }
+
   handleSubmit(event) {
     event.preventDefault()
-    Meteor.call('links.insert', this.refs.input.value)
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+      if(error) {
+        this.setState({ error: 'Enter a valid URL' })
+      } else {
+        this.setState({ error: '' })
+        this.refs.link.value = ''
+      }
+    })
   }
 
   render() {
@@ -12,8 +24,9 @@ class LinkCreate extends Component {
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input ref="input" className="form-control" />
+          <input ref="link" className="form-control" />
         </div>
+        <div className="text-danger">{this.state.error}</div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
     )
