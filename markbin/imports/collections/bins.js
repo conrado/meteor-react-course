8d6802c65fc8fb0c3e6295136572c50a9570ffe2
Conrado Buhrer <conrado@buhrer.net>
2018-09-06 21:25:48 -0300

    added ability to delete bins

--- markbin/imports/collections/bins.js
+++ markbin/imports/collections/bins.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo'
 
 Meteor.methods({
+
   'bins.insert': function() {
     return Bins.insert({
       createdAt: new Date(),
@@ -8,7 +9,12 @@ Meteor.methods({
       sharedWith: [],
       ownerId: this.userId
     })
+  },
+
+  'bins.remove': function(bin) {
+    return Bins.remove(bin)
   }
+
 })
 
 export const Bins = new Mongo.Collection('bins')
