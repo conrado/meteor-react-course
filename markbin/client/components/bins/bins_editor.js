45673e43b29b6126715bd0399a7bcf891923ad09
Conrado Buhrer <conrado@buhrer.net>
2018-09-11 12:41:14 -0300

    added BinsEditor component

---
+++ markbin/client/components/bins/bins_editor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BinsEditor extends Component {
+
+  render() {
+    console.log(this.props.bin)
+    return(
+      <div>Bins Editor</div>
+    )
+  }
+}
+
+export default BinsEditor
