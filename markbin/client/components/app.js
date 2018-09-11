89b537f5b84d3ea522a27c1ff6bdc3f667ca028a
Conrado Buhrer <conrado@buhrer.net>
2018-09-11 10:28:14 -0300

    added binId to route

--- markbin/client/components/app.js
+++ markbin/client/components/app.js
@@ -6,14 +6,14 @@ import BinsMain from './bins/bins_main'
 import BinsList from './bins/bins_list'
 import { Bins } from '../../imports/collections/bins'
 
-export default (props) => {
+export default () => {
   return (
     <div>
       <Header />
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={BinsList} />
-          <Route path="/bins" component={BinsMain} />
+          <Route path="/bins/:binId" component={BinsMain} />
         </Switch>
       </BrowserRouter>
     </div>
--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class BinsMain extends Component {
 
   render() {
+    console.log(this.props.match.params.binId)
     return (
       <div>BinsMain</div>
     )
