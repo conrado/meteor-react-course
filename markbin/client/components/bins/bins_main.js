25368015698e638d70ccf4d1236b067be4fdf11c
Conrado Buhrer <conrado@buhrer.net>
2018-09-11 13:27:44 -0300

    scaffolded BinsShare component

--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -4,6 +4,7 @@ import { Bins } from '../../../imports/collections/bins'
 
 import BinsEditor from './bins_editor'
 import BinsViewer from './bins_viewer'
+import BinsShare from './bins_share'
 
 class BinsMain extends Component {
 
@@ -15,6 +16,7 @@ class BinsMain extends Component {
       <div>
         <BinsEditor bin={this.props.bin} />
         <BinsViewer bin={this.props.bin} />
+        <BinsShare bin={this.props.bin} />
       </div>
     )
   }
